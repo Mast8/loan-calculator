1beb6b7075a6d27f8d333266536ebc4c7d1af373
mast8 <ronald.miranda.rodriguez@gmail.com>
2025-01-09 15:26:45 -0500

    changed to years

--- app.js
+++ app.js
@@ -18,31 +18,31 @@ clearBtn.addEventListener('click', clearInputAndResult);
 // show payment info
 function showPaymentInfo(){
     let monthlyPayment = calcMonthlyPayment(loanAmount.value, interestRate.value, noOfMonth.value);
-    let yearPayment = calcYearPayment(loanAmount.value, interestRate.value, noOfMonth.value);
+    //let yearPayment = calcYearPayment(loanAmount.value, interestRate.value, noOfMonth.value);
     paymentInfoList[0].innerHTML = `$${loanAmount.value.toLocaleString()}`;
     paymentInfoList[1] .innerHTML = `$${interestRate.value}%`;
-    paymentInfoList[2].innerHTML = noOfMonth.value;
-    //paymentInfoList[3].innerHTML = `$${parseFloat(monthlyPayment).toLocaleString()}`;
-    paymentInfoList[3].innerHTML = `$${parseFloat(yearPayment).toLocaleString()}`;
+    paymentInfoList[2].innerHTML = (noOfMonth.value * 12);
+    paymentInfoList[3].innerHTML = `$${parseFloat(monthlyPayment).toLocaleString()}`;
+    //paymentInfoList[3].innerHTML = `$${parseFloat(yearPayment).toLocaleString()}`;
 
 
 }
 
 function calcMonthlyPayment(amount, i, months){
     interest = (i/100) / 12;
-    let monthlyPayment = (amount * interest * Math.pow(1 + interest, months)) / (Math.pow(1 + interest, months) - 1);
+    let monthlyPayment = (amount * interest * Math.pow(1 + interest, (12*months))) / (Math.pow(1 + interest, (12*months)) - 1);
     return monthlyPayment.toFixed(2);
 }
-
+/*
 function calcYearPayment(amount, i, year){
-    interest = (i/100) ;
+    interest = (i/100) /12;
     //I=P×rn
     //add interest
-    //pmt=P×(r/n)×(1+r/n)n×t(1+r/n)n×t−1
+    //pmt=P×(r/n)×(1+r/n)n×t / (1+r/n)n×t−1
 
-    let monthlyPayment = (amount * (( i/100 )/12) );
+    let monthlyPayment = (amount * (interest ) * Math.pow(1+(interest))) ;
     return monthlyPayment.toFixed(2);
-}
+}*/
 
 function clearInputAndResult(){
     calcForm.reset();
