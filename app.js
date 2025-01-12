4ea9d68aba47fd14d478094d1569653202809852
mast8 <ronald.miranda.rodriguez@gmail.com>
2025-01-12 13:22:40 -0500

    css changes

--- app.js
+++ app.js
@@ -20,9 +20,8 @@ function showPaymentInfo(){
     let monthlyPayment = calcMonthlyPayment(loanAmount.value, interestRate.value, noOfMonth.value);
     numberPayments = noOfMonth.value * 12;
     let total = numberPayments*monthlyPayment;
-    //let yearPayment = calcYearPayment(loanAmount.value, interestRate.value, noOfMonth.value);
     paymentInfoList[0].innerHTML = `$${loanAmount.value.toLocaleString()}`;
-    paymentInfoList[1] .innerHTML = `$${interestRate.value}%`;
+    paymentInfoList[1] .innerHTML = `${interestRate.value}%`;
     paymentInfoList[2].innerHTML = numberPayments;
     paymentInfoList[3].innerHTML = `$${parseFloat(monthlyPayment).toLocaleString()}`;
     paymentInfoList[4].innerHTML = total.toFixed(2);
@@ -35,17 +34,7 @@ function calcMonthlyPayment(amount, i, months){
     let monthlyPayment = (amount * interest * Math.pow(1 + interest, (12*months))) / (Math.pow(1 + interest, (12*months)) - 1);
     return monthlyPayment.toFixed(2);
 }
-/*
-add total payment 
-function calcYearPayment(amount, i, year){
-    interest = (i/100) /12;
-    //I=P×rn
-    //add interest
-    //pmt=P×(r/n)×(1+r/n)n×t / (1+r/n)n×t−1
 
-    let monthlyPayment = (amount * (interest ) * Math.pow(1+(interest))) ;
-    return monthlyPayment.toFixed(2);
-}*/
 
 function clearInputAndResult(){
     calcForm.reset();
